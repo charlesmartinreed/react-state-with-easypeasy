019cb7d5de8dcdf5125bc6adb26d762482c1e5dd
charlesmartinreed <charlesmartinreed@icloud.com>
2019-06-26 18:16:03 -0500

    implement thunk middleware, demonstrate fetching data asynchronously from remote API

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,18 @@
-import React, { Fragment } from "react";
-import { useStore } from "easy-peasy";
+import React, { Fragment, useEffect } from "react";
+import { useStore, useActions } from "easy-peasy";
 import TodoItem from "./TodoItem";
 
 const Todos = () => {
   // import our data, using a Hook, provided by easy-peasy
-
   const todos = useStore(state => state.todos);
+  const fetchTodos = useActions(actions => actions.fetchTodos);
+
+  //   since we're using functional components and hooks we can't use the lifecycle methods - useEffect runs everytime the state is updated but to prevent the loop
+  useEffect(() => {
+    fetchTodos();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Fragment>
       <h1>Todos</h1>
--- src/model.js
+++ src/model.js
@@ -1,26 +1,22 @@
-import { action } from "easy-peasy";
+import { action, thunk } from "easy-peasy";
 import uuid from "uuid";
 
 //model is just an object that has all of our state and objects
 export default {
-  todos: [
-    {
-      id: 1,
-      title: "Take out the trash",
-      completed: true
-    },
-    {
-      id: 2,
-      title: "Go grocery shopping",
-      completed: false
-    },
-    {
-      id: 3,
-      title: "Determine the meaning of life",
-      completed: false
-    }
-  ],
-  //  Actions
+  todos: [],
+  //Thunks - action functions
+  fetchTodos: thunk(async actions => {
+    const res = await fetch(
+      "http://jsonplaceholder.typicode.com/todos?_limit=5"
+    );
+    const todos = await res.json();
+
+    actions.setTodos(todos);
+  }),
+  //  Actions - action reducers
+  setTodos: action((state, todos) => {
+    state.todos = todos;
+  }),
   add: action((state, todo) => {
     todo.id = uuid.v4();
     state.todos = [...state.todos, todo];
